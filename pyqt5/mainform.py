529dca8793223946c4a30266cfb41ae5b8b55c29
LinuxCoffe <17056529@qq.com>
2017-08-07 00:13:49 +0800

    add window icon

--- pyqt5/mainform.py
+++ pyqt5/mainform.py
@@ -1,14 +1,17 @@
 import sys
 from PyQt5.QtWidgets import QApplication,  QWidget
+from PyQt5.QtGui import QIcon
 class MainForm(QWidget):
     def __init__(self):
         super().__init__()
-        self.setUi()
+        self.setUI()
         
-    def setUi(self):
-         self.resize(500, 300)
-         self.move(300,  300)
+    def setUI(self):
+         #self.resize(500, 300)
+         #self.move(300,  300)
+         self.setGeometry(300,  300,  1000,  500)
          self.setWindowTitle("Main Form")
+         self.setWindowIcon(QIcon("icon/mainform.ico"))
          self.show()
          
 if __name__ == "__main__":
